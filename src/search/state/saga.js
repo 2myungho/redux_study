08fd7b0c351db4b44b49f15434add98c5a70cf31
2myungho <dl_audgh@naver.com>
2021-03-11 01:07:06 +0000

    first commit

---
+++ src/search/state/saga.js
@@ -0,0 +1,21 @@
+import {all, put, call, takeEvery} from 'redux-saga/effects';
+import {actions, Types} from './index';
+import {callApi} from '../../common/util/api';
+
+function* fetchAutoComplete({keyword}){
+    const {isSuccess, data} = yield call(callApi, {
+        url: '/user/search',
+        params: {keyword},
+    })
+    
+    console.log(data);
+    if(isSuccess && data){
+        yield put(actions.setValue('autoCompletes', data));
+    }
+}
+
+export default function* (){
+    yield all([
+        takeEvery(Types.FetchAutoComplete, fetchAutoComplete),
+    ])
+}
