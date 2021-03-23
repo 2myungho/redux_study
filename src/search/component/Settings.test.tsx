173525ba0e895d33a512b10f92f25eeecf624252
2myungho <dl_audgh@naver.com>
2021-03-23 00:56:21 +0000

    backup

--- src/search/component/Settings.test.tsx
+++ src/search/component/Settings.test.tsx
@@ -1,20 +1,44 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Settings from './Settings';
 
-test('Setting test', () => {
+test('shoud show Setting button, when render Setting Component', () => {
+  //given
+  const logout = jest.fn(() => '클릭');
+
+  //when
+  render(<Settings logout={logout} />);
+  const settingButton = screen.getByRole('button');
+
+  //then
+  expect(settingButton).toBeDefined();
+});
+
+test('shoud show 로그아웃, when click dropdown button', () => {
+  //given
   const logout = jest.fn(() => '클릭');
   render(<Settings logout={logout} />);
+  const settingButton = screen.getByRole('button');
 
-  screen.debug();
-  const button = screen.getByRole('button');
-  expect(button).toBeDefined();
-  fireEvent.click(button);
+  //when
+  fireEvent.click(settingButton);
 
-  screen.debug();
-  // const li = screen.getByText('로그아웃');
-  const li = screen.getByRole('menuitem');
-  expect(li).toBeDefined();
-  fireEvent.click(li);
+  //then
+  const logoutMenu = screen.getByRole('menuitem');
+  expect(logoutMenu).toBeDefined();
+});
 
-  console.log(logout.mock.results);
+test('shoud call logout function, when click 로그아웃', () => {
+  //given
+  const logout = jest.fn(() => '클릭');
+  render(<Settings logout={logout} />);
+  const settingButton = screen.getByRole('button');
+  fireEvent.click(settingButton);
+  const logoutMenu = screen.getByRole('menuitem');
+  expect(logoutMenu).toBeDefined();
+
+  //when
+  fireEvent.click(logoutMenu);
+
+  //then
+  expect(logout).toBeCalled();
 });
